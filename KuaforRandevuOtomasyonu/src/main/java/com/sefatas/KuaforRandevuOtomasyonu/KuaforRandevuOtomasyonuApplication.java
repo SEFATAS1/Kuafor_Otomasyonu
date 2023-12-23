c8293392ca1778723abaff508d6d67bf461a4390
SEFATAS1 <93452541+SEFATAS1@users.noreply.github.com>
2023-12-24 00:32:44 +0300

    First commit
    The project was created via Spring Initializr.

---
+++ KuaforRandevuOtomasyonu/src/main/java/com/sefatas/KuaforRandevuOtomasyonu/KuaforRandevuOtomasyonuApplication.java
@@ -0,0 +1,13 @@
+package com.sefatas.KuaforRandevuOtomasyonu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuaforRandevuOtomasyonuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuaforRandevuOtomasyonuApplication.class, args);
+	}
+
+}
