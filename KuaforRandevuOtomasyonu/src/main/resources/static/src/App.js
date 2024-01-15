46a00a5e5e8956d0e929e561f55d1ea24ed0dbb7
Hasan Ertaş <86931225+hasanertass@users.noreply.github.com>
2024-01-15 04:41:30 +0300

    login sayfa tasarımı -1

--- KuaforRandevuOtomasyonu/src/main/resources/static/src/App.js
+++ KuaforRandevuOtomasyonu/src/main/resources/static/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/admin/header';
 import Footer from './components/admin/footer';
 import Modal from './components/admin/Modal';
-import AdminSidebar from './components/admin/sidebar'; 
-import KuaforSidebar from './components/kuafor/sidebar'; 
-import KullaniciSidebar from './components/kullanici/sidebar'; 
+import AdminSidebar from './components/admin/sidebar';
+import KuaforSidebar from './components/kuafor/sidebar';
+import KullaniciSidebar from './components/kullanici/sidebar';
 import './vendor/fontawesome-free/css/all.min.css';
 import './css/sb-admin-2.min.css';
 import './vendor/datatables/dataTables.bootstrap4.min.css';
@@ -23,47 +23,62 @@ import Randevularim from './pages/kuafor/randevularim';
 import Kuaforler from './pages/kullanici/kuaforler';
 import RandevularimKullanici from './pages/kullanici/randevularim';
 
+import UnauthrizedPage from './pages/UnauthorizedPage';
+import LoginPage from './pages/loginPage';
+
 
 function App() {
-  const userRole = 'kuafor';  
+  const userRole = '';
   const elementToRender =
-  userRole === 'kuafor' ? (
-    <AdminHome />
-  ) : userRole === 'kuafor' ? (
-    <KuaforHome />
-  ) : (
-    <KullaniciHome />
-  );
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <body id="page-top">
-          <div id="wrapper">
-            {userRole==='admin' ? <AdminSidebar/> : ''}
-            {userRole==='kuafor' ? <KuaforSidebar/> : ''}
-            {userRole==='kullanici' ? <KullaniciSidebar/> : ''}
-            <div id="content-wrapper" class="d-flex flex-column">
-              <div id="content">
-                <Topbar/>
-                <div class="container-fluid">
-                  <Routes>
-                    <Route path="/" element={elementToRender} />
-                    <Route path="/kuaforlistesi" element={<KuaförListesi />} />
-                    <Route path="/randevularim" element={<Randevularim />} />
-                    <Route path="/ayarlarim" element={<Ayarlar />} />
-                    <Route path="/randevuistekleri" element={<Randevuİstekleri />} />
-                    <Route path="/kuaforler" element={<Kuaforler />} />
-                    <Route path="/randevularimm" element={<RandevularimKullanici />} />
-                  </Routes>
-                </div>
+    userRole === 'kuafor' ? (
+      <AdminHome />
+    ) : userRole === 'kuafor' ? (
+      <KuaforHome />
+    ) : userRole === 'kullanici' ? (
+      <KullaniciHome />
+    ) : userRole === '' ? (<Navigate to="/login" replace={true} />) : (<UnauthrizedPage />);
+
+  const body = (
+    <div className="App">
+      <Header />
+      <body id="page-top">
+        <div id="wrapper">
+          {userRole === 'admin' ? <AdminSidebar /> : ''}
+          {userRole === 'kuafor' ? <KuaforSidebar /> : ''}
+          {userRole === 'kullanici' ? <KullaniciSidebar /> : ''}
+          <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+              <Topbar />
+              <div class="container-fluid">
+                <Routes>
+                  <Route path="/" element={elementToRender} />
+                  <Route path="/kuaforlistesi" element={userRole === 'admin' ? <KuaförListesi /> : <UnauthrizedPage />} />
+                  <Route path="/randevularim" element={userRole === 'kuafor' ? <Randevularim /> : <UnauthrizedPage />} />
+                  <Route path="/ayarlarim" element={userRole === 'kuafor' ? <Ayarlar /> : <UnauthrizedPage />} />
+                  <Route path="/randevuistekleri" element={userRole === 'kuafor' ? <Randevuİstekleri /> : <UnauthrizedPage />} />
+                  <Route path="/kuaforler" element={userRole === 'kullanici' ? <Kuaforler /> : <UnauthrizedPage />} />
+                  <Route path="/randevularimm" element={userRole === 'kullanici' ? <RandevularimKullanici /> : <UnauthrizedPage />} />
+                </Routes>
               </div>
             </div>
           </div>
-        </body>
-        <Footer/>
-        <Modal/>
-      </div>
+        </div>
+      </body>
+      <Footer />
+      <Modal />
+    </div>
+  );
+  const loginbody = (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
+  return (
+    <Router>
+      {userRole !== '' ? body : loginbody}
     </Router>
   );
 }
---
+++ KuaforRandevuOtomasyonu/src/main/resources/static/src/pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const login = () => {
+  return (
+    <div className="container-fluid">
+      <h1 className="display-2 text-center text-primary mt-5">Yetkisizsiniz</h1>
+      <p className="text-center">Lütfen giriş yapmak için <a href="/login">buraya tıklayın</a>.</p>
+    </div>
+  );
+};
+
+export default login;
